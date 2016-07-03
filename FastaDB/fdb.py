af23f73ff96bfb2bab89ba58f84c294ec92b5436
inacio-medeiros <boyinacio@gmail.com>
2016-07-03 16:49:54 -0300

    Implementing json parsing
    - Implements "FDBDict" class, which stores information about a single gene, and has a method called "build dictionary", which builds a dictionary structure upon class attributes;
    - Implements "store_register" method, for storing registers in a array of registers;
    - Implements "generate_fdb_file_header" and  "mount_fdb_file", for generation of json fdb format file;
    - Modifies FastaToFDB\ to really mount a json fdb format file

--- FastaDB/fdb.py
+++ FastaDB/fdb.py
@@ -1,5 +1,8 @@
 # FASTADB - A DATABASE FOR YOUR FASTA FILES
 
+from fdb_registers import FDBRegister
+import json
+
 
 class FastaDB():
     def __init__(self):
@@ -19,28 +22,60 @@ class FastaDB():
 
         return file
 
+    def generate_fdb_file_header(self):
+        dict_header = {}
+        dict_header['FastaDB'] = 'GENOME.FDB'
+        return dict_header
+
+    def mount_fdb_file(self, fdb_registers):
+        if fdb_registers is None:
+            return None
+
+        dict_header = self.generate_fdb_file_header()
+        fdb_file_dicts = [dict_header]
+
+        for register in fdb_registers:
+            fdb_file_dicts.append(register.build_dictionary())
+
+        return json.dumps(fdb_file_dicts)
+
+    def store_register(self, fdb_registers, fdb_register):
+        """
+        Stores a register in fdb_registers
+        """
+        if (fdb_registers is not None) and (fdb_register is not None):
+            fdb_registers.append(fdb_register)
+
     def FastaToFDB(self, fastafile):
+        fdb_registers = []
+        fdb_register = FDBRegister()
         content = open(fastafile, "r")
-        # fdbformat = {}
+
         for line in content:
             if line.startswith(">") is True:
-                print("COORDENADA")
+                self.store_register(fdb_registers, fdb_register)
+                fdb_register = FDBRegister()
+                fdb_register.filename = fastafile
+                fdb_register.description = line
 
-            if line.startswith(";") is True:
-                print("Comentário")
+            elif line.startswith(";") is True:
+                fdb_register.observations = line
 
-            if line.startswith(";") is False and line.startswith(">") is False:
-                print("sequencia")
+            else:
+                fdb_register.gene = fdb_register.gene + line
+
+        return self.mount_fdb_file(fdb_registers)
 
     def ImportFasta(self, fastafile):
         try:
             # a = open(fastafile, "r")
             # f = open(self.filename, "r+")
             # print(a.read())
-            FastaDB().FastaToFDB(fastafile)
+            return FastaDB().FastaToFDB(fastafile)
         except ValueError:
             return ValueError
 
 FDB = FastaDB()
 FDB.DB("file3.fdb")
-FDB.ImportFasta("test.fasta")
+fdb_file = FDB.ImportFasta("test2.fasta")
+print(fdb_file)
---
+++ FastaDB/fdb_registers.py
@@ -0,0 +1,83 @@
+# DATA STRUCTURES WHICH ORGANIZE FDB FILES
+
+from datetime import date
+
+
+class FDBRegister():
+    def __init__(self):
+        self._description = ""
+        self._gene = ""
+        self._geneinfo = ""
+        self._filename = ""
+        self._date = date.today()
+        self._user = ""
+        self._observations = ""
+
+    def build_dictionary(self):
+        dictionary = {}
+
+        dictionary["description"] = self._description
+        dictionary["gene"] = self._gene
+        dictionary["geneinfo"] = self._geneinfo
+        dictionary["filename"] = self._filename
+        dictionary["date"] = str(self._date)
+        dictionary["user"] = self._user
+        dictionary["observations"] = self._observations
+
+        return dictionary
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, description):
+        self._description = description
+
+    @property
+    def gene(self):
+        return self._gene
+
+    @gene.setter
+    def gene(self, gene):
+        self._gene = gene
+
+    @property
+    def geneinfo(self):
+        return self._geneinfo
+
+    @geneinfo.setter
+    def geneinfo(self, geneinfo):
+        self._geneinfo = geneinfo
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @filename.setter
+    def filename(self, filename):
+        self._filename = filename
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, date):
+        self._date = date
+
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, user):
+        self._user = user
+
+    @property
+    def observations(self):
+        return self._observations
+
+    @observations.setter
+    def observations(self, observations):
+        self._observations = observations

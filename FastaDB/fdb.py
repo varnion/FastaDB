1edc0b18f194d33a12a6f2f7e526ce0d7a00fda6
Vinicius Mesel <vinicius@bigsp.com.br>
2016-07-12 00:25:11 -0300

    InacioMDRS files!
    FastaDB file structure almost done!

--- FastaDB/fdb.py
+++ FastaDB/fdb.py
@@ -1,5 +1,9 @@
 # FASTADB - A DATABASE FOR YOUR FASTA FILES
-from Bio.SeqIO import FastaIO, parse
+
+from Bio.SeqIO import parse
+from fdb_registers import FDBRegister
+import json
+
 
 class FastaDB():
     def __init__(self):
@@ -19,24 +23,48 @@ class FastaDB():
 
         return file
 
-    def FastaToFDB(self, fastafile):
-        sequences = parse(open(fastafile), 'fasta')
-        for fasta in sequences:
-            name, sequence = fasta.id, str(fasta.seq)
-            print(name, sequence)
+    def generate_fdb_file_header(self):
+        dict_header = {}
+        dict_header['FastaDB'] = 'GENOME.FDB'
+        return dict_header
 
+    def mount_fdb_file(self, fdb_registers):
+        if fdb_registers is None:
+            return None
+
+        dict_header = self.generate_fdb_file_header()
+        fdb_file_dicts = [dict_header]
+
+        index = 1
+        for register in fdb_registers:
+            new_dict = {}
+            new_dict["gene"+str(index)] = register.build_dictionary()
+            fdb_file_dicts.append(new_dict)
+            index = index + 1
+
+        return json.dumps(fdb_file_dicts)
+
+    def FastaToFDB(self, fastafile):
+        fdb_registers = []
+        content = open(fastafile)
+
+        sequences = parse(content, 'fasta')
+
+        for sequence in sequences:
+            fdb_register = FDBRegister()
+            fdb_register.filename = fastafile
+            fdb_register.description = sequence.id
+            fdb_register.gene = str(sequence.seq)
+
+            fdb_registers.append(fdb_register)
+
+        content.close()
+
+        return self.mount_fdb_file(fdb_registers)
 
     def ImportFasta(self, fastafile):
         try:
-            a = open(fastafile, "r")
-            f = open(self.filename, "r+")
-            #print(a.read())
-            FastaDB().FastaToFDB(fastafile)
+            return FastaDB().FastaToFDB(fastafile)
         except ValueError:
             return ValueError
 
-
-
-FDB = FastaDB()
-FDB.DB("file3.fdb")
-FDB.ImportFasta("test.fasta")

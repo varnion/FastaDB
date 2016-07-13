ac593c738ea50380e03608d537e9d025c024d721
Inácio Medeiros <boyinacio@gmail.com>
2016-07-13 12:07:29 -0300

    removes useless code from tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,6 @@
 # TODO - Simple tests for checking the code verasity
 # TODO - Make the tests run on Jenkins environment
-# import os
-# import unittest
 from FastaDB import fdb
-
 
 def CreateDB(self):
 	cursor = fdb()
